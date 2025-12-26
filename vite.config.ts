f844a6e8f9b00f1550f05dbb74e204776b3ed3cb
Gonzalo Patino <gpatinoc92@gmail.com>
2025-12-26 07:03:51 -0800

    fix: restore base path for project site

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // For GitHub Pages user site: gonzalopatino.github.io
+  base: '/gpatino/', // For GitHub Pages project site: gonzalopatino.github.io/gpatino/
   build: {
     outDir: 'dist',
     sourcemap: false,
